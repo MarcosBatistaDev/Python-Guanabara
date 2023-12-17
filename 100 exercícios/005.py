c96b48ac3f2aec43871350e18651a9f123a9d95c
Marcos da Silva Batista <marcosbatsita20programmingdev@gmail.com>
2023-12-17 18:14:49 -0300

    exercício 5
    Sucessor e antecessor de um número

---
+++ 100 exercícios/005.py
@@ -0,0 +1,4 @@
+#  Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+num = int(input("Informe um número: "))
+print(f"O número {num} tem como sucessor {num + 1} e antecessor {num - 1}")
