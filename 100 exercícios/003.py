4210b151ed4d2fc98bf3bea1265c03868002e3c0
Marcos da Silva Batista <marcosbatsita20programmingdev@gmail.com>
2023-12-17 12:42:20 -0300

    exercício 3
    Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ 100 exercícios/003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+valor_1 = float(input("Digite o primeiro valor: "))
+valor_2 = float(input("Digite o segundo valor: "))
+soma = valor_1 + valor_2
+print(f"{valor_1} + {valor_2} = {soma}")
