906e145022a14add19e15701c6b7a13e1445673e
Marcos da Silva Batista <marcosbatsita20programmingdev@gmail.com>
2023-12-17 20:52:47 -0300

    exercício 7
    # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.

---
+++ 100 exercícios/007.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+nota_1 = float(input("Nota 1: "))
+nota_2 = float(input("Nota 2: "))
+media = (nota_1 + nota_2) / 2
+print(f"Sua média é de: {media}")
