879aabc00fb66f80b36e6e71925a9cf98b17e3a9
Marcos da Silva Batista <marcosbatsita20programmingdev@gmail.com>
2023-12-17 20:47:31 -0300

    exercício 6
    # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

---
+++ 100 exercícios/006.py
@@ -0,0 +1,6 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+num = float(input("Digite um número: "))
+print(f"Dobro: {num * 2}")
+print(f"triplo: {num * 3}")
+print(f"raiz quadrada: {num ** (1/2)}")
