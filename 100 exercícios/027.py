4f60ed99dbf628365d8d76137dd01296af924565
Marcos da Silva Batista <marcosbatsita20programmingdev@gmail.com>
2023-12-21 06:05:10 -0300

    exercício 27
    Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.

---
+++ 100 exercícios/027.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nome = str(input("Nome completo: ")).title().split()
+print(f"Seu primeiro nome é {nome[0]}")
+print(f"Seu último nome é {nome[-1]}")
