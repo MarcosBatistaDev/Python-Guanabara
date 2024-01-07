7f1570b2af46535422094a035772b6c90044ebe7
Marcos da Silva Batista <marcosbatsita20programmingdev@gmail.com>
2024-01-07 12:04:41 -0300

    exercício 72
    Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.

---
+++ 100 exercícios/072.py
@@ -0,0 +1,8 @@
+# Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+
+num = int(input('Digite um número entre 0 e 20: '))
+while num > 20 or num < 0:
+    num = int(input('Número inválido. Digite um número entre 0 e 20: '))
+print(f'Você digitou o número {numeros[num]}')
